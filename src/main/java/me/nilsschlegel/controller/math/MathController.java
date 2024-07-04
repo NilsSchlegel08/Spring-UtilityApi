3463deddf80de0eb5d922ee20ad9a82d23961e05
Truthy <steamnilsoo@gmail.com>
2024-07-04 12:01:58 +0200

    Added MathController.java

--- src/main/java/me/nilsschlegel/controller/math/MathController.java
+++ src/main/java/me/nilsschlegel/controller/math/MathController.java
@@ -13,20 +13,37 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/*
+ * MathController is a REST controller that provides endpoints for mathematical operations.
+ */
+
 @RestController
 @RequestMapping("/math")
 public class MathController {
 
+    /*
+     * Returns the value of pi.
+     */
     @GetMapping("/pi")
     public ResponseEntity<Map<String, Double>> getPi() {
         return ResponseEntity.ok(Map.of("pi", Math.PI));
     }
 
+
+    /*
+     * Returns the value of e.
+     */
     @GetMapping("/e")
     public ResponseEntity<Map<String, Double>> getE() {
         return ResponseEntity.ok(Map.of("e", Math.E));
     }
 
+
+    /*
+     * Returns the square root of a given number.
+     * @param number, The number to calculate the square root of.
+     */
     @GetMapping("/sqrt/{number}")
     public ResponseEntity<Map<String, Double>> getSqrt(
       @PathVariable("number") double number
