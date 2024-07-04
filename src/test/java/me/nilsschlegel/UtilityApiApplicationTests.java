e3c3376493944e513603d3bd42fe25a2b6c1c4dd
Truthy <steamnilsoo@gmail.com>
2024-07-04 11:18:50 +0200

    Added useless Test

--- src/test/java/me/nilsschlegel/UtilityApiApplicationTests.java
+++ src/test/java/me/nilsschlegel/UtilityApiApplicationTests.java
@@ -12,17 +12,30 @@
 
 package me.nilsschlegel;
 
+import java.util.Random;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Repeat;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class UtilityApiApplicationTests {
 
-    @Test
-    void contextLoads() {
-        /*
-         " TODO: Write Tests.
-         */
-    }
+    private final Random random = new Random();
 
+    @Test
+    @DisplayName("Context loads")
+    @Repeat(5)
+    void contextLoads() {
+        boolean current = random.nextBoolean();
+
+        if (current) {
+            assertThat(current).isTrue();
+        } else {
+            assertThat(current).isFalse();
+        }
+    }
 }
