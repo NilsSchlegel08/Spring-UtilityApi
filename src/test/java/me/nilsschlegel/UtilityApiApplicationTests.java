087d91cc15ca7e5ba62dd30fe89ca5275fa46169
Truthy <steamnilsoo@gmail.com>
2024-07-04 09:38:36 +0200

    Initial commit

---
+++ src/test/java/me/nilsschlegel/UtilityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nilsschlegel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
